b6f1cb24c24b9a1cbe8dbfccbc44dd658186145c
Sophie Meier <mesophie@gmx.ch>
2017-07-03 13:37:37 -0400

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  #get 'ask', to:"coachings#answer"
+  post 'answer', to:"coachings#answer"
+
+
+  get 'answer', to: 'coachings#answer'
+  #get 'coaching', to: "coachings#answer"
+
+  get 'back', to: "coachings#ask"
+
+  #get 'coachings/ask'
+  get 'ask', to:"coachings#ask"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
